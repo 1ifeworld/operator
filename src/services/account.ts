e04be178f73f1ead0c726fabc9a62bdc0849122d
Salief <salieflewis@gmail.com>
2023-10-15 12:54:09 -0400

    add and run biome formatting cmd

--- src/services/account.ts
+++ src/services/account.ts
@@ -5,13 +5,13 @@ const router = Router()
 
 router.post('/create-account', (req, res) => {
   if (req.body == null) {
-    res.status(400).json({ error: 'Bad request, body is missing'})
+    res.status(400).json({ error: 'Bad request, body is missing' })
     return
   }
-  const { initialAdmin } = req.body;
+  const { initialAdmin } = req.body
 
   if (!isAddress(initialAdmin)) {
-    res.status(400).json({ error: 'Invalid address'})
+    res.status(400).json({ error: 'Invalid address' })
   }
 
   // TODO: Add UO building functionality, ensure address doesn't already exist in the database
