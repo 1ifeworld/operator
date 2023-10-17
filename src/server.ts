52aacb1feb906dc91056ccf89ad3262ee580dc92
jawndiego <jawndiego@gmail.com>
2023-10-17 13:52:28 -0400

    set up verification via unkey

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import cors from 'cors'
 import express from 'express'
+import { Unkey } from '@unkey/api'
+import { verifyKey } from '@unkey/api'
 import { router as createAccountRouter } from '@/services'
 
 const PORT = process.env.PORT ?? 8080
 
 const app = express()
+const unkey = new Unkey({ rootKey: `${process.env.UNKEY_ROOT}` });
+
 
 const corsOptions = {
     origin: 'https://auth-demo-bice.vercel.app/',
@@ -12,7 +16,30 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.use('/create-account', createAccountRouter)
+
+app.use('/create-account', async (req, res) => {
+  const apiKey = req.headers.authorization?.replace("Bearer ", "");
+  if (!apiKey) {
+    return res.status(401).send("Unauthorized");
+  }
+  try {
+    const { result, error } = await verifyKey(apiKey);
+
+    if (error) {
+      console.error(error);
+      return res.status(500).json("Internal Server Error");
+    }
+
+    if (!result.valid) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    return res.json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json("Internal Server Error");
+  }
+});
 
 app.listen(PORT)
 
